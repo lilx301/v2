859d88ee69b08fb17357131f23b62855b8b9c794
liw003 <liw003>
2025-08-21 09:07:17 +0800

    vv

--- t0.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const tool = require("./tool.js");
-
-(async function(){
-    
-   
-})() 
-
-
--- task.sh
+++ task.sh
@@ -1,13 +1,3 @@
-node t0.js
 echo "\n\n-----------------v2ex.js----------------";
 node v2ex.js
-# echo "\n\n-----------------pgy.js----------------";
-# node pgy.js
-echo "\n\n-----------------sign2.js----------------";
-node sign2.js
-# echo "\n\n-----------------t2.js----------------";
-# node t2.js
-echo "\n\n-----------------producst js----------------";
-node kxproducts.js
-
 
